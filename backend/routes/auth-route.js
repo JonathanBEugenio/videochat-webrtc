8c3b17132b0058970260862e01f2e6b6cb0aa813
Jonathan Brendon Eugênio <jonathan.eugenio@leucotron.com.br>
2022-03-24 21:57:48 -0300

    add validation to auth requests body using joi

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -1,9 +1,21 @@
+const joi = require('joi');
 const express = require('express');
+const validator = require('express-joi-validation').createValidator({});
 const { loginController, registerController } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/register', loginController);
-router.post('/login', registerController);
+const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(6).max(12).required()
+});
+const registerSchema = joi.object({
+    email: joi.string().email().required(),
+    username: joi.string().min(3).max(12).required(),
+    password: joi.string().min(6).max(12).required()   
+});
+
+router.post('/login', validator.body(loginSchema), loginController);
+router.post('/register', validator.body(registerSchema), registerController);
 
 module.exports = router;
