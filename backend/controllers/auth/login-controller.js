c1ab2c84362f43e8ce48a8318f59340fd627134c
Jonathan Brendon Eugênio <jonathan.eugenio@leucotron.com.br>
2022-05-10 21:40:59 -0300

    fix typo

--- backend/controllers/auth/login-controller.js
+++ backend/controllers/auth/login-controller.js
@@ -3,48 +3,48 @@ const bcryptjs = require("bcryptjs");
 const User = require("../../models/user-model");
 
 const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        // find user by email
-        const user = await User.findOne({email: email.toLowerCase()});
+    // find user by email
+    const user = await User.findOne({ email: email.toLowerCase() });
 
-        if(!user) {
-            // http status not found
-            return res.status(404).send('Invalid e-mail.');
-        }
-
-        // compare password with hashed one
-        const passwordValid = await bcryptjs.compare(password, user.password);
-
-        if (!passwordValid) {
-            // http status unauthorized
-            return res.status(401).send('Invalid password.');
-        }
-
-        // create jwt
-        const token = jwt.sign(
-            {
-                email,
-                userId: user._id
-            },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: '24h'  
-            }
-        );
-
-        res.status(200).json({
-            userDatails: {
-                token,
-                email: user.email,
-                username: user.username
-            }
-        });
-    } catch (error) {
-        // http status internal server error 
-        return res.status(500).send('Internal server error.');
+    if (!user) {
+      // http status not found
+      return res.status(404).send('Invalid e-mail.');
     }
+
+    // compare password with hashed one
+    const passwordValid = await bcryptjs.compare(password, user.password);
+
+    if (!passwordValid) {
+      // http status unauthorized
+      return res.status(401).send('Invalid password.');
+    }
+
+    // create jwt
+    const token = jwt.sign(
+      {
+        email,
+        userId: user._id
+      },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: '24h'
+      }
+    );
+
+    res.status(200).json({
+      userDetails: {
+        token,
+        email: user.email,
+        username: user.username
+      }
+    });
+  } catch (error) {
+    // http status internal server error 
+    return res.status(500).send('Internal server error.');
+  }
 };
 
 module.exports = login;
--- backend/controllers/auth/register-controller.js
+++ backend/controllers/auth/register-controller.js
@@ -3,50 +3,50 @@ const bcryptjs = require('bcryptjs');
 const User = require('../../models/user-model');
 
 const register = async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
+  try {
+    const { email, username, password } = req.body;
 
-        // check if user already exists
-        const userExists = await User.exists({ email: email.toLowerCase() });
+    // check if user already exists
+    const userExists = await User.exists({ email: email.toLowerCase() });
 
-        if(userExists) {
-            // http status conflict
-            return res.status(409).send('E-mail already in use.')            
-        }
-
-        // encrypt password
-        const encryptedPassword = await bcryptjs.hash(password, 10);
-        
-        // create document and save it in database
-        const user = await User.create({
-            username,
-            password: encryptedPassword,
-            email: email.toLowerCase()
-        });
-
-        // create jwt
-        const token = jwt.sign(
-            {
-                email,
-                userId: user._id
-            },
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: '24h'  
-            }
-        );
-
-        return res.status(201).json({
-            userDatails: {
-                token,
-                email: user.email,
-                username: user.username
-            }
-        });
-    } catch (error) {
-        // http status internal server error 
-        return res.status(500).send('Internal server error.');
+    if (userExists) {
+      // http status conflict
+      return res.status(409).send('E-mail already in use.')
     }
+
+    // encrypt password
+    const encryptedPassword = await bcryptjs.hash(password, 10);
+
+    // create document and save it in database
+    const user = await User.create({
+      username,
+      password: encryptedPassword,
+      email: email.toLowerCase()
+    });
+
+    // create jwt
+    const token = jwt.sign(
+      {
+        email,
+        userId: user._id
+      },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: '24h'
+      }
+    );
+
+    return res.status(201).json({
+      userDetails: {
+        token,
+        email: user.email,
+        username: user.username
+      }
+    });
+  } catch (error) {
+    // http status internal server error 
+    return res.status(500).send('Internal server error.');
+  }
 };
 
 module.exports = register;
